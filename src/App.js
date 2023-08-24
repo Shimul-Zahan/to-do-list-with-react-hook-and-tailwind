17b7337e89d1c3afd31abe8ab0c030caa0af9bb0
AlgoWebBot <shimulzahan636@gmail.com>
2023-08-25 00:23:32 +0600

    input and button add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto bg-yellow-100 min-h-screen'>
+      <h1 className='text-center text-5xl font-bold pt-4'>Hello React How are you?</h1>
+      <div className='text-center pt-4'>
+        <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+        <button className="btn btn-outline btn-primary">Primary</button>
+      </div>
     </div>
   );
 }
